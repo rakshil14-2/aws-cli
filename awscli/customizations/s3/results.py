228a4565757188867370f9e1ab22cb404813cf8a
Rakshil Modi <rhmodi@amazon.com>
2025-08-13 15:54:28 -0700

    Fix Bug

--- awscli/customizations/s3/results.py
+++ awscli/customizations/s3/results.py
@@ -518,7 +518,9 @@ class ResultPrinter(BaseResultHandler):
 
     def _clear_progress_if_no_more_expected_transfers(self, **kwargs):
         if self._progress_length and not self._has_remaining_progress():
-            uni_print(self._adjust_statement_padding(''), self._out_file)
+            clear_statement = '\r' + ' ' * self._progress_length + '\n'
+            uni_print(clear_statement, self._out_file)
+            self._progress_length = 0
 
 
 class NoProgressResultPrinter(ResultPrinter):
